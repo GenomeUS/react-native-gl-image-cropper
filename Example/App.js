a7301d38e5f50b6614b87d19f622fe5a6b95b835
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2019-03-06 17:57:43 -0500

    Add eslint and fix module

--- Example/App.js
+++ Example/App.js
@@ -1,55 +1,53 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { View, TouchableOpacity, Text, Image } from 'react-native';
 import { ImageCropper } from 'react-native-gl-image-cropper';
 
+
+const image = 'https://images.unsplash.com/photo-1551741520-d8a61f499d25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: 'https://images.unsplash.com/photo-1551741520-d8a61f499d25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      image,
       height: 200,
       width: 300,
       zoom: 0,
-      showNew: true,
-      newImage: 'https://images.unsplash.com/photo-1551741520-d8a61f499d25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      newImage: image,
     };
   }
 
-  render() {
-    return (
-      
-        <View style={{ flex: 1, backgroundColor: '#fff' }}>
-          <ImageCropper
-            ref={(ref) => {this.cropper = ref;}}
-            image={this.state.image}
-            cropHeight={this.state.height}
-            cropWidth={this.state.width}
-            maxZoom={80}
-            minZoom={0}
-            panToMove={true}
-            pinchToZoom={true}
-          />
-          <View style={{flex: 1, marginTop: 20}}>
-            <TouchableOpacity onPress={this.capture}>
-              <Text style={{color: "gray", padding: 10 }}>CAPTURE</Text>
-            </TouchableOpacity>
-
-            <Image source={{ uri: this.state.newImage }} style={{height: this.state.height, width: this.state.width}} />
-
-          </View>
-        </View>
-      
-    );
-  }
-  capture = () => {
+  capture = () => (
     this.cropper.crop()
-    .then(res =>{
-      this.setState({
-        showNew: true,
-        newImage: res,
-      });
-    })
-  }
-}
+      .then(res => (
+        this.setState({
+          newImage: res,
+        })
+      ))
+  )
 
+  render = () => (
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <ImageCropper
+        ref={(ref) => { this.cropper = ref; }}
+        image={this.state.image}
+        cropHeight={this.state.height}
+        cropWidth={this.state.width}
+        maxZoom={80}
+        minZoom={0}
+        zoom={this.state.zoom}
+        panToMove
+        pinchToZoom
+      />
+      <View style={{ flex: 1, marginTop: 20 }}>
+        <TouchableOpacity onPress={this.capture}>
+          <Text style={{ color: 'gray', padding: 10 }}>CAPTURE</Text>
+        </TouchableOpacity>
+        <Image
+          source={{ uri: this.state.newImage }}
+          style={{ height: this.state.height, width: this.state.width }}
+        />
+      </View>
+    </View>
+  );
+}
--- src/GLImage.js
+++ src/GLImage.js
@@ -1,5 +1,5 @@
-import GL from "gl-react";
-import React from "react";
+import GL from 'gl-react';
+import React from 'react';
 import rectCrop from 'rect-crop';
 import rectClamp from 'rect-clamp';
 import PropTypes from 'prop-types';
@@ -11,7 +11,7 @@ const shaders = GL.Shaders.create({
     varying vec2 uv;
     uniform sampler2D t;
     uniform vec4 crop;
-    vec2 invert (vec2 p) {${""/* y is reversed in gl context */}
+    vec2 invert (vec2 p) {
       return vec2(p.x, 1.0-p.y);
     }
     void main () {
@@ -22,84 +22,55 @@ const shaders = GL.Shaders.create({
       step(p.x, 1.0) *
       step(p.y, 1.0) *
       texture2D(t, p);
-    }`
-  }
+    }`,
+  },
 });
 
-const GLImage = GL.createComponent(
-({
+const GLImage = GL.createComponent(({
   width,
   height,
   source,
   imageSize,
-  resizeMode = "cover",
   center,
   zoom,
 }) => {
-  if (!imageSize) {
+  let imageSizeValue = imageSize;
+  let centerValue = center;
+  let zoomValue = zoom;
+  if (!imageSizeValue) {
     if (source.width && source.height) {
-      imageSize = { width: source.width, height: source.height };
+      imageSizeValue = { width: source.width, height: source.height };
+    } else {
+      throw new Error("gl-rect-image: imageSizeValue is required if you don't provide {width,height} in source");
     }
-    else {
-      throw new Error("gl-rect-image: imageSize is required if you don't provide {width,height} in source");
-    }
-  }
-  let crop;
-  switch (resizeMode) {
-  case "cover": {
-    if (!center) center = [ 0.5, 0.5 ];
-    if (!zoom) zoom = 1;
-    let rect = rectCrop(zoom, center)({ width, height }, imageSize);
-    rect = rectClamp(rect, [ 0, 0, imageSize.width, imageSize.height ]);
-    crop = [
-      rect[0] / imageSize.width,
-      rect[1] / imageSize.height,
-      rect[2] / imageSize.width,
-      rect[3] / imageSize.height
-    ];
-    break;
-  }
-  case "contain": {
-    if (center || zoom) {
-      console.warn("gl-react-image: center and zoom props are only supported with resizeMode='cover'");
-    }
-    const ratio = width / height;
-    const imageRatio = imageSize.width / imageSize.height;
-    crop =
-      ratio > imageRatio
-      ? [ (1 - ratio / imageRatio) / 2, 0, ratio / imageRatio, 1 ]
-      : [ 0, (1 - imageRatio / ratio) / 2, 1, imageRatio / ratio ];
-    break;
-  }
-  case "stretch":
-    if (center || zoom) {
-      console.warn("gl-react-image: center and zoom props are only supported with resizeMode='cover'");
-    }
-    crop = [ 0, 0, 1, 1 ];
-    break;
-
-  default:
-    throw new Error("gl-react-image: unknown resizeMode="+resizeMode);
   }
 
-  return <GL.Node
-    shader={shaders.image}
-    uniforms={{
-      t: source,
-      crop,
-    }}
-  />;
-},
-  {
-    displayName: "GLImage",
-    propTypes: {
-      source: PropTypes.any.isRequired,
-      imageSize: PropTypes.shape({
-        width: PropTypes.number.isRequired,
-        height: PropTypes.number.isRequired,
-      }),
-      resizeMode: PropTypes.string,
-    }
-  });
+  if (!center) centerValue = [0.5, 0.5];
+  if (!zoomValue) zoomValue = 1;
+  let rect = rectCrop(zoomValue, centerValue)({ width, height }, imageSizeValue);
+  rect = rectClamp(rect, [0, 0, imageSizeValue.width, imageSizeValue.height]);
+  const crop = [
+    rect[0] / imageSizeValue.width,
+    rect[1] / imageSizeValue.height,
+    rect[2] / imageSizeValue.width,
+    rect[3] / imageSizeValue.height,
+  ];
+
+  return (
+    <GL.Node
+      shader={shaders.image}
+      uniforms={{ t: source, crop }}
+    />
+  );
+}, {
+  displayName: 'GLImage',
+  propTypes: {
+    source: PropTypes.any.isRequired,
+    imageSizeValue: PropTypes.shape({
+      width: PropTypes.number.isRequired,
+      height: PropTypes.number.isRequired,
+    }),
+  },
+});
 
 export default GLImage;
